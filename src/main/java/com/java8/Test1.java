3bf0de3294c06e5429050d654f2c98f78bfb8533
zhongming.xu <zhongming.xu@wormpex.com>
2018-03-16 00:09:34 +0800

    update

--- src/main/java/com/java8/Test1.java
+++ src/main/java/com/java8/Test1.java
@@ -1,9 +1,8 @@
 package com.java8;
 
-import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 
-import java.util.stream.Stream;
+import java.util.List;
 
 /**
  * TODO completion javadoc.
@@ -13,10 +12,12 @@ import java.util.stream.Stream;
  */
 public class Test1 {
 
-    public static void main(String[] args){
-        String[] array = {"a", "b", "c"};
-        for(Integer i : Lists.newArrayList(1,2,3)){
-            Stream.of(array).map(item -> Strings.padEnd(item, i, '@')).forEach(System.out::println);
-        }
+    public static void main(String[] args) {
+//        String[] array = {"a", "b", "c"};
+//        for(Integer i : Lists.newArrayList(1,2,3)){
+//            Stream.of(array).map(item -> Strings.padEnd(item, i, '@')).forEach(System.out::println);
+//        }
+        List<String> stringList = Lists.newArrayList();
+        stringList.get(0);
     }
 }
