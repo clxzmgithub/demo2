0fbb19196fd3b18be1b4e328cc1abb8106bc2df5
zhongming.xu <zhongming.xu@wormpex.com>
2018-03-29 07:57:09 +0800

    设计模式之禅 命令模式 责任链模式

--- src/main/java/com/design_pattern_meditation/command_and_duty_chain_model/version1/ClassUtils.java
+++ src/main/java/com/design_pattern_meditation/command_and_duty_chain_model/version1/ClassUtils.java
@@ -3,9 +3,14 @@ package com.design_pattern_meditation.command_and_duty_chain_model.version1;
 /**
  * @author xuzhongming
  * @since 26 三月 2018
- *
+ * <p>
  * 根据父类获得子类
  */
 public class ClassUtils {
 
+    public static void main(String[] args) {
+//        BigDecimal bigDecimal = new BigDecimal(0.95);
+//        int a = bigDecimal.intValue();
+//        System.out.println(a);
+    }
 }
