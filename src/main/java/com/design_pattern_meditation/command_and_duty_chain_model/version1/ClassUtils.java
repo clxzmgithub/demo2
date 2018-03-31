070ae35ab655514b00472b3d9bb1d8b7dec749e6
zhongming.xu <zhongming.xu@wormpex.com>
2018-03-31 14:27:21 +0800

    设计模式之缠 命令模式和责任链模式

--- src/main/java/com/design_pattern_meditation/command_and_duty_chain_model/version1/ClassUtils.java
+++ src/main/java/com/design_pattern_meditation/command_and_duty_chain_model/version1/ClassUtils.java
@@ -7,17 +7,5 @@ package com.design_pattern_meditation.command_and_duty_chain_model.version1;
  * 根据父类获得子类
  */
 public class ClassUtils {
-
-    public static void main(String[] args) {
-//        BigDecimal bigDecimal = new BigDecimal(0.95);
-//        int a = bigDecimal.intValue();
-//        System.out.println(a);
-//        double a = 32.44 / 35.24;
-//        System.out.println(a);
-
-        double a = 7;
-        double b = 6;
-//        String str = "" + a - b + "";
-        System.out.println("aaaa" + (a - b) + "bbbb");
-    }
+    
 }
---
+++ src/main/java/com/design_pattern_meditation/command_and_duty_chain_model/version1/Test.java
@@ -0,0 +1,24 @@
+package com.design_pattern_meditation.command_and_duty_chain_model.version1;
+
+/**
+ * @author xuzhongming
+ * @since 26 三月 2018
+ * <p>
+ * 根据父类获得子类
+ */
+public class Test {
+
+    public static void main(String[] args) {
+//        BigDecimal bigDecimal = new BigDecimal(0.95);
+//        int a = bigDecimal.intValue();
+//        System.out.println(a);
+//        double a = 32.44 / 35.24;
+//        System.out.println(a);
+//        double a = 7;
+//        double b = 6;
+//        String str = "" + a - b + "";
+//        System.out.println("aaaa" + (a - b) + "bbbb");
+
+
+    }
+}
